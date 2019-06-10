1a499a90c4aca682d8c9347a580d61da72b9189c
Nico Bermudez <bermnicolas@gmail.com>
2019-06-10 15:16:31 -0400

    Fixed headers for Face++ API

--- client/src/reducers/moodReducer.js
+++ client/src/reducers/moodReducer.js
@@ -1,14 +1,13 @@
 export default function moodReducer (state= {
   mood: "Happy",
 }, action) {
-  debugger
   switch(action.type) {
 
     case 'CHANGE_MOOD':
       return { mood: action.mood }
-      
+
     case 'FACIAL_RECOGNITION_CHANGE_MOOD':
-      let facial_recognition_emotions = action.mood.faces.attributes.emotion
+      let facial_recognition_emotions = action.mood.faces[0].attributes.emotion
       let max_emotion = Object.keys(facial_recognition_emotions).reduce((a, b) => facial_recognition_emotions[a] > facial_recognition_emotions[b] ? a : b);
       return { mood: max_emotion }
 
